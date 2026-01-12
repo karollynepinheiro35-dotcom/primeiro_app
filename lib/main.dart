8cdd810ff9a558df35fb4713b100f06f423c21f2
KGPN <kgpn@freud.karoginny@gmail.com>
2026-01-11 23:27:36 -0400

    CI iOS Kyra (GitHub Actions)

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Meu primeiro app Flutter esta vivo',
+              '',
             ),
             Text(
               '$_counter',
